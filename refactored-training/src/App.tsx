7922bbfc5108f936dba9bb48a4a2a754c64bbf37
davidsl <david.lange@geodata.no>
2025-06-09 16:04:48 +0200

    Header component

--- refactored-training/src/App.tsx
+++ refactored-training/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './Home/Home'
 import About from './About/About'
@@ -6,23 +6,20 @@ import Events from './Events/Events'
 import Places from './Places/Places'
 import Contact from './Contact/Contact'
 import Game from './Game/Game'
+import Header from './Header/Header'
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{' '}
-        <Link to="/events">Events</Link> | <Link to="/places">Places</Link> |{' '}
-        <Link to="/contact">Contact</Link> | <Link to="/game">Game</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/places" element={<Places />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/game" element={<Game />} />
-      </Routes>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/places" element={<Places />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
     </Router>
   )
 }
---
+++ refactored-training/src/Header/Header.tsx
@@ -0,0 +1,16 @@
+import styles from './Header.module.css'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <div className={styles.headerContainer}>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{' '}
+        <Link to="/events">Events</Link> | <Link to="/places">Places</Link> |{' '}
+        <Link to="/contact">Contact</Link> | <Link to="/game">Game</Link>
+      </nav>
+    </div>
+  )
+}
+
+export default Header;
