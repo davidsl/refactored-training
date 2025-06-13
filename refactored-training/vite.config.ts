81b497052de94c3555d8410ac7149b200a23e4d7
davidsl <david.lange@geodata.no>
2025-06-13 22:23:35 +0200

    update vite config

--- refactored-training/vite.config.ts
+++ refactored-training/vite.config.ts
@@ -5,6 +5,7 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/refactored-training/', // <-- THIS IS IMPORTANT!
   plugins: [react(), mkcert()],
   server: {
     https: true,
