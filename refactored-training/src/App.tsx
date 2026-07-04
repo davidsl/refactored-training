7044e7ce6e1734d8e9be599f9c40891adec2a7a9
davidsl <david.lange@geodata.no>
2026-07-04 14:54:02 +0200

    refactor: remove unused components and styles for Events, Contact, and Places; update About section layout and styling

--- refactored-training/src/App.tsx
+++ refactored-training/src/App.tsx
@@ -2,9 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './Home/Home'
 import About from './About/About'
-import Events from './Events/Events'
-import Places from './Places/Places'
-import Contact from './Contact/Contact'
 import Minesweeper from './Minesweeper/Minesweeper'
 import Leaderboard from './Leaderboard/Leaderboard'
 import Header from './Header/Header'
@@ -20,9 +17,6 @@ function App() {
         <Routes>
           <Route path="/refactored-training" element={<Home />} />
           <Route path="/refactored-training/about" element={<About />} />
-          <Route path="/refactored-training/events" element={<Events />} />
-          <Route path="/refactored-training/places" element={<Places />} />
-          <Route path="/refactored-training/contact" element={<Contact />} />
           <Route path="/refactored-training/game" element={<Minesweeper />} />
           <Route path="/refactored-training/leaderboard" element={<Leaderboard />} />
           <Route path="/refactored-training/styling-examples" element={<StylingExamples />} />
--- refactored-training/src/Header/Header.tsx
+++ refactored-training/src/Header/Header.tsx
@@ -21,9 +21,6 @@ function Header() {
       <nav id="main-nav" className={`${styles.navMenu} ${menuOpen ? styles.navOpen : styles.navClosed}`}>
         <Link to="/refactored-training" onClick={() => setMenuOpen(false)}>Home</Link>
         <Link to="/refactored-training/about" onClick={() => setMenuOpen(false)}>About</Link>
-        <Link to="/refactored-training/events" onClick={() => setMenuOpen(false)}>Events</Link>
-        <Link to="/refactored-training/places" onClick={() => setMenuOpen(false)}>Places</Link>
-        <Link to="/refactored-training/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
         <Link to="/refactored-training/game" onClick={() => setMenuOpen(false)}>Minesweeper</Link>
         <Link to="/refactored-training/leaderboard" onClick={() => setMenuOpen(false)}>Leaderboard</Link>
         <Link to="/refactored-training/styling-examples" onClick={() => setMenuOpen(false)}>Styling Examples</Link>
